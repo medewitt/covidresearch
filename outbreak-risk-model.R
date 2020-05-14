5d415e16bfe27de63eef7286d853854dd7654592
Michael DeWitt <me.dewitt.jr@gmail.com>
2020-05-14 06:41:12 -0400

    adding outbreak functions

--- outbreak-risk-model.R
+++ outbreak-risk-model.R
@@ -7,15 +7,22 @@ diff.Date(c(as.Date("2020-03-18"), Sys.Date()))
 generate_convolution <- function(n_sim=1000, lambda = 8/56, mu = 40, 
 																 sigma = 10, alpa_tol = .05, 
 																 covar = .3, 
-																 severity_func = "gamma", severity_cap = 100){
+																 severity_func = "gamma", 
+																 severity_cap = 100){
 	
-	severity_function <- match.arg(severity_func, c("gamma", "pareto"))
+	severity_function <- match.arg(severity_func, c("gamma", "pareto", "weibull"))
 	if(severity_func=="gamma"){
 		# Severity
 		sigma.sq <- sigma^2
 		beta <- mu/sigma.sq
 		alpha <- beta * mu
 		severity <- rgamma(n_sim, alpha, beta)
+	} else if (severity_func =="weibull"){
+		
+		k <- (sigma/mu)^(-1.086)
+		c <- mu/(gamma(1+1/k))
+		severity <- rweibull(n_sim,k,c)
+		
 	} else{
 		
 		severity <- rmutil::rpareto(n = n_sim, m = mu, s = 100000)
@@ -41,15 +48,19 @@ generate_convolution <- function(n_sim=1000, lambda = 8/56, mu = 40,
 # gamma severity function -------------------------------------------------
 
 loss <- generate_convolution(lambda = 8/56, covar = 0)
-
+op <-par(mgp=c(1.5,.5, 0), tck=-.01, 
+				 mar=c(4,4,3,3), col.main = "#012169", col.sub = "#012169",
+				 col.axis = "grey20", col.lab = "grey20", fg = "grey20")
+op
+par(mfrow = c(3,1))
 summary(loss[["loss"]])
 hist(loss[["loss"]], breaks = 30, right = FALSE,
 		 col = "#00A2C2", 
-		 xlab = "Numer of Patients",
+		 xlab = "Number of Patients",
 		 main = "Likely Positive Congregate Living Residents per Day", 
 		 adj=0, xaxs="i", xaxt="n", bty="l")
 axis(1, seq(0,40,2), cex.axis=1.1, cex.lab=1.1)
-mtext(text = "Using a Gamma Outbreak Function", side = 3, adj = 0)
+mtext(text = "Using a Gamma Outbreak Function", side = 3, adj = 0, col = "#012169")
 
 
 # pareto severity function ------------------------------------------------
@@ -60,15 +71,25 @@ summary(loss[["loss"]])
 summary(loss[["severity"]])
 hist(loss[["loss"]], breaks = 30, right = FALSE,
 		 col = "#00A2C2", 
-		 xlab = "Numer of Patients",
+		 xlab = "Number of Patients",
 		 main = "Likely Positive Congregate Living Residents per Day", 
 		 adj=0, xaxs="i", xaxt="n", bty="l")
 axis(1, seq(0,40,2), cex.axis=1.1, cex.lab=1.1)
-mtext(text = "Using a Pareto Outbreak Function", side = 3, adj = 0)
+mtext(text = "Using a Pareto Outbreak Function", side = 3, adj = 0, col = "#012169")
 
-k <- (sd(rw)/mean(rw))^(-1.086)
-c <- mean(rw)/(gamma(1+1/k))
 
-k <- (20/mean(40))^(-1.086)
-c <- mean(40)/(gamma(1+1/k))
-hist(rweibull(100,k,c))
+# weibull -----------------------------------------------------------------
+loss <- generate_convolution(lambda = 8/56, covar = 0, 
+														 severity_func = "weibull")
+
+summary(loss[["loss"]])
+summary(loss[["severity"]])
+hist(loss[["loss"]], breaks = 30, right = FALSE,
+		 col = "#00A2C2", 
+		 xlab = "Number of Patients",
+		 main = "Likely Positive Congregate Living Residents per Day", 
+		 adj=0, xaxs="i", xaxt="n", bty="l")
+axis(1, seq(0,40,2), cex.axis=1.1, cex.lab=1.1)
+title(main = "", sub = "Yes")
+#mtext(text = "Using a Weibull Outbreak Function", side = 3, adj = 0, col = "#012169")
+
