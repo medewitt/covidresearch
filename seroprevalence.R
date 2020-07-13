00f7ce348e1e5344f5a9263f375977a9efb2b583
Michael DeWitt <me.dewitt.jr@gmail.com>
2020-07-13 15:41:42 -0400

    adding stuff

---
+++ seroprevalence.R
@@ -0,0 +1,46 @@
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+model <- stan_model(here::here("seroprevalence.stan"))
+
+# Wake Forest Data
+# USe Abbot Testing Platform Data From:
+# Evaluation of sensitivity and specificity of 
+#four commercially available SARS-CoV-2 antibody
+# Oxford University Hospitals Study
+
+
+
+postitives <- 18750*.14
+dat <- list(
+	y_sample = as.integer(postitives),
+	n_sample = 18750L,
+	sensitivity = .927,
+	specificity = .999,
+	sensitivity_sd = .01,
+	specificity_sd= .001
+)
+
+fit <- sampling(model, data = dat, iter = 2000,
+								control = list(adapt_delta = .95))
+
+summary(fit)
+
+# change testing params ---------------------------------------------------
+
+dat <- list(
+	y_sample = as.integer(postitives),
+	n_sample = 18750L,
+	sensitivity = .927,
+	specificity = .999,
+	sensitivity_sd = .01,
+	specificity_sd= .001
+)
+
+fit <- sampling(model, data = dat, iter = 2000,
+								control = list(adapt_delta = .95))
+
+summary(fit)$summary
+
+stan_plot(fit, pars = "p")
