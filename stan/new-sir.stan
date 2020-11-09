014eccedcddbff1acdba61ada3fdf086098a89ab
Michael DeWitt <me.dewitt.jr@gmail.com>
2020-11-09 07:12:25 -0500

    new format for ode solvers in stan

--- stan/new-sir.stan
+++ stan/new-sir.stan
@@ -3,17 +3,13 @@
 functions {
   vector simple_SIR(real t,
                     vector y,
-                    real beta,    // water contact rate
-                    real kappa,   // C_{50}
-                    real gamma,   // recovery rate
-                    real xi,      // bacteria production rate
-                    real delta) { // bacteria removal rate
-    vector[4] dydt;
+                    real beta,    //  contact rate
+                    real gamma) { // bacteria removal rate
+    vector[3] dydt;
 
-    dydt[1] = -beta * y[4] / (y[4] + kappa) * y[1];
-    dydt[2] = beta * y[4] / (y[4] + kappa) * y[1] - gamma * y[2];
+    dydt[1] = -beta * y[1] *y[2]/sum(y);
+    dydt[2] = beta * y[1] *y[2]/sum(y) - gamma * y[2];
     dydt[3] = gamma * y[2];
-    dydt[4] = xi * y[2] - delta * y[4];
 
     return dydt;
   }
@@ -24,6 +20,9 @@ data {
   int<lower=0> N_t;
   real t[N_t];
   vector[3] y0;
+  int cases[N_t-1];
+  int pred_window;
+  real t_pred[N_t+pred_window];
 }
 
 transformed data {
@@ -33,24 +32,35 @@ transformed data {
 parameters {
   real<lower=0> beta;
   real<lower=0> gamma;
-  real<lower=0> delta;
+  real<lower=0> phi_inv;
 }
 
 transformed parameters {
+  real<lower=0> phi = 1. / phi_inv;
+  real incidence[N_t-1];
   vector<lower=0>[3] y[N_t] = ode_rk45_tol(simple_SIR, y0, t0, t,
 					   1e-6, 1e-6, 1000,
-					   beta, gamma, xi, delta);
+					   beta, gamma);
+	for (i in 1:(N_t-1))
+  	incidence[i] = y[i, 1] - y[i + 1, 1];
 }
   
 model {
-  vector[N_t] y_diff;
-  y_diff[1] = y0[1] - y[1, 1];
-  for (n in 2:N_t)
-    y_diff[n] = y[n - 1, 1] - y[n, 1];
   
+  cases ~ neg_binomial_2(incidence, phi);
   beta ~ cauchy(0, 2.5);
   gamma ~ cauchy(0, 1);
-  delta ~ cauchy(0, 1);
+  phi_inv ~ exponential(1);
 
 }
-
+generated quantities{
+  real incidence_out[N_t+pred_window-1];
+  vector<lower=0>[3] y_pred[N_t+pred_window] = ode_rk45_tol(simple_SIR, y0, t0, t_pred,
+					   1e-6, 1e-6, 1000,
+					   beta, gamma);
+	for(i in 1:(N_t+pred_window-1)){
+	  incidence_out[i] = y_pred[i,1] - y_pred[i+1,1];
+	}
+	
+					   
+}
---
+++ stan/sir-v-2.R
@@ -0,0 +1,104 @@
+library(cmdstanr)
+
+mod <- cmdstan_model(here::here("stan", "new-sir.stan"))
+
+
+# fake data ---------------------------------------------------------------
+library(deSolve)
+sir_model = function (current_timepoint, state_values, parameters)
+{
+	# create state variables (local variables)
+	S = state_values [1]        # susceptibles
+	I = state_values [2]        # infectious
+	R = state_values [3]        # recovered
+	N = state_values [1] + state_values [2] + state_values [3]
+	
+	with ( 
+		as.list (parameters),     # variable names within parameters can be used 
+		{
+			# compute derivatives
+			dS = (-beta * S * I)/N
+			dI = (beta * S * I)/N  - (gamma * I)
+			dR = (gamma * I)
+			
+			# combine results
+			results = c (dS, dI, dR)
+			list (results)
+		}
+	)
+}
+
+beta_value <- 1/3
+gamma_value <- 1/10
+
+parameter_list <- c (beta = beta_value, gamma = gamma_value)
+times <- 1:120
+initial_values <- c(S= 1000-2, I =2, R = 0)
+output = as.data.frame(lsoda (initial_values, 
+															times, sir_model, 
+															parameter_list))
+
+output <- dplyr::mutate_if(output, is.numeric, round)
+
+cases <- vector(mode = "numeric")
+for(i in 1:25){
+	cases[i]<- output[i,2]-output[i+1,2]
+}
+
+# stan_data ---------------------------------------------------------------
+slice_n <- 25
+stan_data <- list(
+	N_t = as.integer(slice_n),
+	y0 = c(998,2,0),
+	t = 1:slice_n,
+	cases = cases[1:24],
+	pred_window = 14,
+	t_pred = 1:(slice_n+14)
+)
+fit <- mod$sample(data = stan_data, 
+									chains = 2, parallel_chains = 2,
+									iter_sampling = 50)
+fit$summary()
+library(tidybayes)
+library(posterior)
+library(dplyr)
+library(tidyverse)
+library(ggdist)
+tidy_draws.CmdStanMCMC <- function(model, ...) {
+	return(as_draws_df(model$draws()))
+}
+fit$draws(variables = c("incidence"))
+fit$draws(variables = c("incidence_out"))
+
+gather_draws(fit, incidence[i]) %>%
+	dplyr::ungroup() %>% 
+	group_by(i) %>%
+	curve_interval(.value, .width = c(.5, .8, .95)) %>%
+	ggplot(aes(x = i, y = .value)) +
+	geom_hline(yintercept = 1, color = "gray75", linetype = "dashed") +
+	geom_lineribbon(aes(ymin = .lower, ymax = .upper))+
+	scale_fill_brewer() +
+	labs(
+		title = "Simulated SIR Curve for Infections",
+		y = "Cases"
+	)+
+	geom_point(data = tibble(cases = cases, i = 1:length(cases)),
+						 aes(i, cases), inherit.aes = FALSE, colour = "orange")+
+	theme_minimal()
+	
+fit$summary(variables = "incidence_out")
+gather_draws(fit, incidence_out[i]) %>%
+	dplyr::ungroup() %>% 
+	group_by(i) %>%
+	curve_interval(.value, .width = c(.5, .8, .95)) %>%
+	ggplot(aes(x = i, y = .value)) +
+	geom_hline(yintercept = 1, color = "gray75", linetype = "dashed") +
+	geom_lineribbon(aes(ymin = .lower, ymax = .upper))+
+	scale_fill_brewer() +
+	labs(
+		title = "Simulated SIR Curve for Infections",
+		y = "Cases"
+	)+
+	geom_point(data = tibble(cases = cases, i = 1:length(cases)),
+						 aes(i, cases), inherit.aes = FALSE, colour = "orange")+
+	theme_minimal()
