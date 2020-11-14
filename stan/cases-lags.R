2cc076c83c5953354c2741e551db6a1a3ece6a32
Michael DeWitt <me.dewitt.jr@gmail.com>
2020-11-14 11:54:53 -0500

    i love gps

--- stan/cases-lags.R
+++ stan/cases-lags.R
@@ -10,8 +10,7 @@ dat <-nccovid::pull_covid_summary()
 dat <- dat[!is.na(daily_cases)]
 
 dat_red <- dat[,.(date,daily_cases,daily_deaths)] %>% 
-	.[ ,daily_deaths:=fifelse(is.na(daily_deaths),0,daily_deaths)] %>% 
-	.[date>as.Date("2020-03-20")]
+	.[ ,daily_deaths:=fifelse(is.na(daily_deaths),0,daily_deaths)]
 
 corz <- vector(mode = "numeric")
 for(i in 1:30){
@@ -25,6 +24,20 @@ plot(corz, type = "b", pch = 19,
 abline(v = which.max(corz), lty = 2)
 abline(v = which.max(corz), lty = 2)
 
-plot(dat_red$daily_cases, type = "b", pch = 19, 
+plot(shift(dat_red$daily_cases,6, fill = 0)*.01, type = "b", pch = 19, ylim = c(0,70),
 		 xlab = "Lag", ylab = "Correlation",
 		 main = "Lags Between Reported Cases and Deaths in NC", adj = 0)
+lines(dat_red$daily_deaths, col = "red")
+
+under <- dat_red %>% 
+	.[daily_cases > 0] %>% 
+	.[,daily_cfr := daily_deaths/daily_cases] %>% 
+	.[,undercount := daily_cfr/.008] %>% 
+	.[,date_n := as.numeric(date)]
+fit <- mgcv::gam(undercount ~ s(date_n,bs = "gp"), 
+								 data = under,
+								 weights = daily_cases)
+plot(fitted(fit), type = "b", pch = 19)
+abline(h = 1,  col = "firebrick", lty = 2)
+abline(h = 2,  col = "firebrick", lty = 3)
+
